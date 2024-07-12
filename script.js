9c49189e27130ff237dcaba736b757fa640a4635
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-07-12 12:09:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function fibonacci(num) {
 // your code here
+	if (num <= 0) {
+		throw new ERROR("Input must be a postive integer")
+	}
+
+	if (num == 1) {
+		return 0;
+	}
+
+	if (num == 2) {
+		return 1;
+	}
+
+	let a = 0;
+	let b = 1;
+
+	for (let i = 3; i <= num i++) {
+		let temp = a + b;
+		a = b;
+		b = temp;
+	}
+
+	return b;
 }
 
 module.exports = fibonacci;
